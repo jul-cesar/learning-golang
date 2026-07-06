8eb14d1278054652d171b15b9d27e08695dba5a8
Julio Cesar <juliocesarmartinez1344@gmail.com>
2026-07-05 20:00:49 -0500

    enhance documentation in main.go and notes.md with additional explanations

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
+/* every Go program starts with a package declaration. The package name is the first line of every Go source file. The package main is obligatory for executable programs, the compler will search for the main function */
 package main
 
+// this is the syntax of an import statement, it is used to import packages from the standard library or from other packages. Something important abount import in the main package is that it cant import anything from the project but it cant be imported by other packages //
 import (
 	"fmt"
 )
 
+/* this is the syntax of a function, with the func keyword, very similar to other languages like TypeScript */
+
+// the main function is the entry point of the program, it is called when the program starts executing, in this case we use it to print Hello world using de fmt (format) package from Go //
 func main() {
 	fmt.Println("Hello world")
 }
--- notes.md
+++ notes.md
@@ -5,3 +5,12 @@ Designed by goole in 2007, publicly announced in 2009 and version 1.0 realeased 
 Know for its simplicit, efficiency and strng support for concurrent programming
 
 it created to make software developers life "easier"
+
+
+
+#Run Golang project or file
+
+to run a golang file we use the command go run and the name of the file, in this case go run main.go
+
+
+if we want it to be compile it to binary we use go build -o main.go
