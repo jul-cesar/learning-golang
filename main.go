cddabc14cc26cf0ef3013616df6cc6aec7dfd4a5
Julio César Martinez <juliocesarmartinez1344@gmail.com>
2026-07-08 12:43:19 -0500

    refactor main.go to remove slice examples and enhance map documentation with detailed explanations

--- main.go
+++ main.go
@@ -6,30 +6,38 @@ import (
 
 func main() {
 
-	// slices
+	// Maps
 
-	numbers := []int{1, 2, 3, 4, 5}
-	// this is how you can create a slice from an array or another slice
-	allNumbers := numbers[:]
-	fmt.Printf("All numbers: %v\n", allNumbers)
-	// this takes the first three numbers from the slice
-	firstThreeNumbers := numbers[0:3]
-	fmt.Printf("First three numbers: %v\n", firstThreeNumbers)
+	// Maps in golang are unordered collections of key-value pairs. They are similar to dictionaries in Python or hash tables in other programming languages. Maps are useful for storing and retrieving data based on unique keys.
 
-	// this is how you can create new slices
-	fruits := []string{
-		"apple",
-		"banana",
-		"cherry",
+	// to create a map we can use the map keyword
+	// The argument to the map keyword is the type of the key and the type of the value. For example, to create a map that maps strings to integers, we can use the following syntax:
+	// myMap := map[string]int{})
+	CapitalCities := map[string]string{
+		"France":  "Paris",
+		"Germany": "Berlin",
+		"Italy":   "Rome",
 	}
-	fmt.Printf("Fruits: %v\n", fruits)
+	fmt.Printf("Capital cities: %v\n", CapitalCities)
 
-	// we cand append to a slice using the built-in append function
-	fruits = append(fruits, "date")
-	fmt.Printf("Fruits after appending: %v\n", fruits)
+	// To retrieve a value from a map, we can use the key as an index. For example, to get the capital city of France, we can use the following syntax:
+	capital := CapitalCities["France"]
+	fmt.Printf("The capital of France is: %s\n", capital)
 
-	for i, v := range fruits {
-		fmt.Printf("Index: %d, Value: %s\n", i, v)
+	// If the value doesnt not exist, the zero value of the value type will be returned. For example, if we try to get the capital city of Spain, which is not in the map, we will get an empty string:
+	capital = CapitalCities["Spain"]
+	fmt.Printf("The capital of Spain is: %s\n", capital)
+
+	// To check if a key exists in a map, we can use the two-value assignment syntax. The second value will be a boolean indicating whether the key exists in the map. For example:
+	capital, ok := CapitalCities["Spain"]
+
+	if ok {
+		fmt.Printf("The capital of Spain is: %s\n", capital)
+	} else {
+		fmt.Println("Spain is not in the map")
 	}
 
+	// to delete a key-value pair from a map, we can use the delete function. For example, to delete the capital city of Germany from the map, we can use the following syntax:
+
+	delete(CapitalCities, "Germany")
 }
