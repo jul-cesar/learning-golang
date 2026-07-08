efd40c881815ab5e64d7d3dc7e1ff4f6f0a0c7a9
Julio César Martinez <juliocesarmartinez1344@gmail.com>
2026-07-08 10:38:11 -0500

    enhance main.go with detailed comments explaining package, import, and function syntax

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
-/* every Go program starts with a package declaration. The package name is the first line of every Go source file. The package main is obligatory for executable programs, the compler will search for the main function */
 package main
 
-// this is the syntax of an import statement, it is used to import packages from the standard library or from other packages. Something important abount import in the main package is that it cant import anything from the project but it cant be imported by other packages //
 import (
 	"fmt"
 )
 
-/* this is the syntax of a function, with the func keyword, very similar to other languages like TypeScript */
 
-// the main function is the entry point of the program, it is called when the program starts executing, in this case we use it to print Hello world using de fmt (format) package from Go //
 func main() {
-	fmt.Println("Hello world")
+	// For loop that iterates 5 times
+	for i := 0; i < 5; i++ {
+		fmt.Println("This is iteration number:", i)
+	}
+
+	// In goglang theres no while loop, but you can use a for loop to achieve the same functionality
+	counter := 0
+	for counter < 5 {
+		fmt.Println("Counter is at:", counter)
+		counter++
+	}
+	// Infinite loop
+	for {
+		break // This will break the infinite loop immediately
+	}
+
+	
+
 }
