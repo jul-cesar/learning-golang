e7a803b78618e06d0add250f1e2c44e4bdafa4ff
Julio César Martinez <juliocesarmartinez1344@gmail.com>
2026-07-08 11:16:44 -0500

    refactor main.go to remove unused loop examples and enhance array documentation

--- main.go
+++ main.go
@@ -4,24 +4,21 @@ import (
 	"fmt"
 )
 
-
 func main() {
-	// For loop that iterates 5 times
-	for i := 0; i < 5; i++ {
-		fmt.Println("This is iteration number:", i)
-	}
 
-	// In goglang theres no while loop, but you can use a for loop to achieve the same functionality
-	counter := 0
-	for counter < 5 {
-		fmt.Println("Counter is at:", counter)
-		counter++
-	}
-	// Infinite loop
-	for {
-		break // This will break the infinite loop immediately
-	}
+	// Arrays 
+	// the firt parameter is the length of the array and the second parameter is the type of the array
+	numbers := [5]int{1, 2, 3, 4, 5}
+	// to print an array we can use the %v verb in fmt.Printf
+	fmt.Printf("This is the array %v\n", numbers)
+	// with len() function we can get the length of an array
+	fmt.Println("The array length is", len(numbers))
 
-	
-
+	// multidimensional arrays
+	// the first parameter is the length of the array and the second parameter is the length of the inner array and the third parameter is the type of the array+
+	multi := [2][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+	fmt.Printf("This is the multidimensional array %v\n", multi)
 }
