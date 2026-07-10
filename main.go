13126f6a2102581994ada572bd49bdde37541a7b
Julio César Martinez <juliocesarmartinez1344@gmail.com>
2026-07-10 10:56:31 -0500

    refactor main.go to implement modifyPerson as a method on Person struct and enhance pointer usage examples

--- main.go
+++ main.go
@@ -27,16 +27,27 @@ func main() {
 
 	fmt.Printf("This is a person struct: %+v\n", person)
 
-	// Anonymous structs are useful when you need a struct for a specific purpose and don't want to define a new type.
-	anonymousStruct := struct {
-		height int
-		weight int
-	}{
-		height: 180,
-		weight: 75,
-	}
-	fmt.Printf("This is an anonymous struct: %+v\n", anonymousStruct)
+	fmt.Println("Name before modification:", person.Name)
+	// Pass the address of the person struct to the modifyPerson function to modify its Name field. Using & allows us to pass a pointer to the struct, so we can modify the original struct instead of a copy.
+	person.modifyPerson()
+	fmt.Println("Name after modification:", person.Name)
 
-	
+	x := 10
+	ptr := &x // Create a pointer to the variable x
+	fmt.Println("Value of x>: %t and address: %p", x, ptr)
 
+	*ptr = 20 // Modify the value of x using the pointer
+	fmt.Println("Value of x after modification using pointer:", x) 
+}
+
+// The modifyPerson function takes a pointer to a Person struct as an argument. It modifies the Name field of the struct and prints the modified value.
+// func modifyPerson(person *Person) {
+// 	person.Name = "Modified Name"
+// 	fmt.Println("Name after modification inside function:", person.Name)
+// }
+
+// Instead of defining the modifyPerson function as a standalone function, we can define it as a method on the Person struct. This allows us to call the method directly on an instance of the Person struct, making the code more readable and organized.
+func (person *Person) modifyPerson() {
+	person.Name = "Modified Name"
+	fmt.Println("Name after modification inside function:", person.Name)
 }
