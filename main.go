80cb3bf81ea9676b57c14cdf85d8eca9419b75b2
Julio Cesar <juliocesarmartinez1344@gmail.com>
2026-08-15 15:56:13 -0500

    Start HTTP application server
    Wire application initialization, routes, database cleanup, and server
    timeouts behind a configurable port flag. Update module metadata and
    dependencies.

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
-/* every Go program starts with a package declaration. The package name is the first line of every Go source file. The package main is obligatory for executable programs, the compler will search for the main function */
 package main
 
-// this is the syntax of an import statement, it is used to import packages from the standard library or from other packages. Something important abount import in the main package is that it cant import anything from the project but it cant be imported by other packages //
 import (
+	"flag"
 	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/jul-cesar/learning-golang/internal/app"
+	"github.com/jul-cesar/learning-golang/internal/routes"
 )
 
-/* this is the syntax of a function, with the func keyword, very similar to other languages like TypeScript */
-
-// the main function is the entry point of the program, it is called when the program starts executing, in this case we use it to print Hello world using de fmt (format) package from Go //
 func main() {
-	fmt.Println("Hello world")
+
+	var port int
+	flag.IntVar(&port, "port", 8080, "Port to run the application on")
+	flag.Parse()
+
+	application, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+	defer  application.Db.Close()
+	
+	r := routes.SetupRoutes(application)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
+		IdleTimeout:  5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	application.Logger.Printf("Server running on port %d", port)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		application.Logger.Fatal(err)
+	}
 }
